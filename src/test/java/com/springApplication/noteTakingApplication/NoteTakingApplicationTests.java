6fe1ba35ebf3afbfb3fa84df266e4f35d389609b
Viraj Jadhav <virajjadhao85@gmail.com>
2024-02-25 12:36:15 +0530

    initial commit

---
+++ src/test/java/com/springApplication/noteTakingApplication/NoteTakingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springApplication.noteTakingApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteTakingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
